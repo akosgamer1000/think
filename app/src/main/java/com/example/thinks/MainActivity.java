51c1358fd6a8f26614e4f81401656075841f186b
Petrik Diak <diak@petrik.hu>
2024-10-08 14:40:30 +0200

    dhgdg

--- app/src/main/java/com/example/thinks/MainActivity.java
+++ app/src/main/java/com/example/thinks/MainActivity.java
@@ -28,6 +28,7 @@ public class MainActivity extends AppCompatActivity {
     Integer rannum;
     Random ran;
     Integer life;
+    Integer limti;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);

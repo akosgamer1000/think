1c515ad0350e692b407b40d684d832be19c393cf
Petrik Diak <diak@petrik.hu>
2024-10-08 14:08:44 +0200

    dhgdg

--- app/src/main/java/com/example/thinks/MainActivity.java
+++ app/src/main/java/com/example/thinks/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.thinks;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,8 +11,20 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.Random;
 
+public class MainActivity extends AppCompatActivity {
+    ImageView s1;
+    ImageView s2;
+    ImageView s3;
+    Button plus;
+    Button minusz;
+    Button submit;
+    TextView number;
+    Integer ques;
+    Integer rannum;
+    Random ran;
+    Integer life;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +35,22 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        innit();
+
+    }
+
+    public void innit(){
+        s1=findViewById(R.id.imageheart);
+        s2=findViewById(R.id.imageheart2);
+        s3=findViewById(R.id.imageheart3);
+        plus=findViewById(R.id.buttonplusz);
+        minusz=findViewById(R.id.buttonminusz);
+        submit=findViewById(R.id.submit);
+        number=findViewById(R.id.number);
+
+        rannum=0;
+        ran=new Random();
+        ques=ran.nextInt(11);
+        life=3;
     }
 }
